2915c34b51491c120cfec59f93baa3b8bc9488a6
miaoshan <minnieshan@gmail.com>
2019-09-10 14:44:07 +0100

    form reset

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Employee } from '../models/employee.model';
 import { EmployeeService } from './employee.service';
